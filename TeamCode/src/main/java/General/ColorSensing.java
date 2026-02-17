70a35f5bac15f5deb4a44084f78c3bb3bff5435a
ka12114242 <aayushkhadka274@gmail.com>
2026-02-17 17:46:12 -0600

    hello

---
+++ TeamCode/src/main/java/General/ColorSensing.java
@@ -0,0 +1,18 @@
+package General;
+
+import com.qualcomm.hardware.rev.RevColorSensorV3;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class ColorSensing extends LinearOpMode {
+
+    private RevColorSensorV3 colorSensor = null;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        colorSensor = hardwareMap.get( RevColorSensorV3.class, "colorSensor");
+
+
+
+
+    }
+}
